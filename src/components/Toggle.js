44e0fba06ba359aa3a42bd61000ddbe790a6bb50
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-13 23:37:10 +0300

    Completed Assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState }from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setisOn] = useState(false);
+
+  function handleClick() {
+    setisOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return <button 
+         style={{ background: color }} 
+         onClick={handleClick}>
+        {isOn ? "ON" : "OFF"}
+        </button>;
 }
 
 export default Toggle;
+
+
+// function Toggle() {
+//   function handleClick(e) {
+//     e.preventDefault();
+//     console.log("clicked");
+//   }
+//   function handleOffClick(e) {
+//     e.preventDefault();
+//     console.log("OFF");
+//   }
+//   return <button onClick={handleClick} onBlur={handleOffClick}>OFF</button>;
+// }
